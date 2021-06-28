30cb544f230e0cbc33d0c59181929bf74036c6b9
Francisco Gonzalez <fgonzalezcaceres@santandertecnologia.com.ar>
2021-06-28 05:58:00 -0300

    Se modifica el DML

--- gonzalez_caceres_francisco_batallaDeGallos_DML.sql
+++ gonzalez_caceres_francisco_batallaDeGallos_DML.sql
@@ -1,8 +1,21 @@
---pg_ctl.exe restart -D  "C:\Program Files\PostgreSQL\9.6\data"
+/*(a) Genere una base de datos en el motor PostgreSQL cuyo nombre sea tp_su_apellido. No desapruebe
+por literalidad. Describa los pasos que tuvo que llevar a cabo para lograrlo. Guarde las sentencias
+que usó para la creación de las tablas en el archivo sql.*/
+--Yo lo hice a traves de un ide. Los pasos que seguí son los siguientes:
+--Nos conectamos a la base a traves del usuario postgresql y la clave que generamos al instalar el motor,
+--y lo apuntamos al localhost con el puerto por defecto que viene asignado(5432). Abrimos una consola y ejecutamos
+--el siguiente script
 CREATE DATABASE tp_gonzalez_caceres;
+--Esto crea una base de datos a partir de la cual crearemos todos los objetos que necesitemos para la realizacion del TP
+--Puede crearse un shema para encapsular el dominio del TP, pero por fines practicos no lo hice.
+--El siguiente script se podría utilizar para tal fin -> CREATE SHEMA batalla_de_gallos;
 
---CREATE SCHEMA batalla_de_gallos;
 
+/*(b) Escriba las queries para crear las tablas y estructuras de acuerdo a lo descripto más arriba. El puntaje de
+las rimas por defecto debe ser 2 y la duración de la temática debe ser por default de 30 segundos.
+(c) Identifique todas las claves foráneas que correspondan y escriba las queries para crearlas.
+(d) Ejecute las queries de modo tal que todas estas estructuras sean creadas en la base de datos creada en el
+punto a.*/
 CREATE TABLE plaza (
     id serial PRIMARY KEY,
     nombre varchar(30) NOT NULL,
@@ -30,7 +43,6 @@ CREATE TABLE competidor (
     CONSTRAINT fk_plaza
       FOREIGN KEY(plaza_id) 
 	    REFERENCES plaza(id)
-
 );
 
 CREATE TABLE tematica (
@@ -55,7 +67,6 @@ CREATE TABLE tematica_en_competicion (
             REFERENCES tematica(id)
 );
 
-
 CREATE TABLE rima (
     valoracion int DEFAULT 2,
     patron integer,
@@ -75,4 +86,35 @@ CREATE TABLE rima (
             REFERENCES tematica(id)
 );
 
+/*(e) Inserte en la base los datos brindados en el archivo datos_batalla_de_gallos.sql. Describa los
+pasos que tuvo que llevar a cabo para lograrlo, qué metodo usó.*/
+
+--Yo realice una ejecución del archivo a traves del IDE, pero se podría hacer con siguiente comando:
+--psql "nombre de la base" <"path adsoluto del archivo" -U "nombre usuario".
+--Como el archivo de insercion poseia atributos no descriptos en el modelo se tuvo que hacer una alter
+--table para poder insertar los mismos.
 ALTER TABLE rima ADD COLUMN fecha_registro date;
+
+/*(f) Limite el atributo patron a un maximo de 4 y el atributo valoracion a un maximo de 10 en la tabla
+rima.*/
+ALTER TABLE rima ADD CONSTRAINT patron_valoracion_check
+    CHECK (patron <= 4 AND valoracion <= 10);
+
+/*(g) Realice la inserción de datos de forma tal que exista una rima de la temática 4x4 en una competición
+hecha en el predio del Luna Park por un competidor de sobrenombre Wolf oriundo de una plaza de
+Bernal, y que dicha rima esté valorada con 7. Dichas queries deben estar en el entregable.*/
+--Estas queries deberian estar en el archivo de DDL pero para mantener un orden las dejo acá
+DO $$
+BEGIN
+    INSERT INTO tematica(id, nombre, descripcion)
+    VALUES (8,'4x4','Todo terreno');
+
+    INSERT INTO plaza (id, nombre, ciudad, provincia)
+    VALUES (12,'Del Maestro', 'Bernal', 'Buenos Aires');
+
+    INSERT INTO competidor(id, sobrenombre, plaza_id,especialidad, es_mayor)
+    VALUES (25,'Wolf', 12, 'Rapear con Querys', false);
+
+    INSERT INTO rima (valoracion, patron, competidor_id, competicion_id, tematica_id, fecha_registro)
+    VALUES (7,1,25,1,8,'2021-03-01');
+END$$;
